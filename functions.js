69388ca68687dab6a4324873fa2c1fa7475784a1
Javmir Gamma <javmir.hernandez@students.gammatech.schoolgit config --global user.email javmir.hernandez@students.gammatech.school>
2024-04-30 21:30:55 +0200

    Comienzo de ejercicios funciones

---
+++ functions.js
@@ -0,0 +1,32 @@
+/* 1.- Escribe una función que acepte la medida de los 
+dos lados de un rectángulo y devuelva el área de dicho rectángulo.
+ Si no se le pasa ningún parámetro, los lados deberán ser, 
+ por defecto, 5 y 3. Puedes usar prompts para los datos (opcional) y
+ la consola del navegador para comprobar el resultado.
+ 
+ */
+
+ let sideArectangle = parseInt(prompt("Ingresa un "))
+ let sideBrectangle = parseInt(prompt("Ingresa un "))
+  
+
+
+ function areaRectangle(a,b) {
+    return a * b;
+ }
+
+ console.log(areaRectangle(sideArectangle, sideBrectangle));
+
+ /* 
+ 2.- Escribe una función que acepte grados Celsius y
+  devuelva la conversión a grados Fahrenheit.
+(0°C × 9/5) + 32 = 32°F
+
+Grados Fahrenheit = (grados celsius × 9/5) +32.
+
+*/
+
+
+function degreesCelsius() {
+    
+} 
